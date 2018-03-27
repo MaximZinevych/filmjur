9247a4c8264f43349c04e5f441653a92ab9db444
Maxim <maximzinevych@gmail.com>
2018-03-27 21:51:59 +0300

    [Task] #2 [Project] com.codecrab.filmjur [Changelog] Added login page

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,17 +4,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MainPageComponent } from './main-page/main-page.component';
+import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent
+  ]
 })
 export class AppModule { }
